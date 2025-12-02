8f791fbacbc2d8a53c48d6b186150b7b97bcafb8
vsspook <166043447+vsspook@users.noreply.github.com>
2025-12-02 21:51:53 +0800

    Add files via upload
    cantor 列表 一道洛谷上的简单题

---
+++ cantor 列表/cantor 列表/cantor 列表.cpp
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>
+int jiecheng(int i) {
+	int n = 1;
+	int sum = 0;
+	while (n <= i) {
+		sum = sum + n;
+		n++;
+	}
+	return sum;
+
+}
+int main() {
+	int count = 0;
+	int num;
+	scanf_s("%d",&num);
+	int result = 0;
+	do{
+		result = jiecheng(count);
+		count++;
+		
+	} while (result < num);
+		count--;
+		result = jiecheng(count);
+
+	int surplus = num - result;
+	int fengzi;
+	int fengmu;
+	if (count % 2 == 0) {
+		fengmu = surplus;
+		fengzi = count + 1;
+	}
+	else {
+		fengmu = count + 2 - surplus;
+		fengzi = surplus;
+	}
+	printf("%d/%d", fengzi, fengmu);
+	return 0;
+
+}
