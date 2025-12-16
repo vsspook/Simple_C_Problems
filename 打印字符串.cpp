181b024aa937079960146a6bb89050eca302297c
Swing.rifff <166043447+vsspook@users.noreply.github.com>
2025-12-16 21:07:17 +0800

    Add files via upload

---
+++ 打印字符串.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+void print(char *a) {
+	int i = 0;
+	while (*(a+i) != '\0') {
+		printf("%c", a[i]);
+		i++;
+	}
+	printf("\n");
+}
+int main() {
+	char c[10] = "Hello";
+	print(c);
+	return 0;
+
+}
