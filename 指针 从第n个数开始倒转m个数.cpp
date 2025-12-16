55871b32edea364cc5f36595dd5ccfc3191183e3
Swing.rifff <166043447+vsspook@users.noreply.github.com>
2025-12-16 21:08:11 +0800

    Add files via upload

---
+++ 指针 从第n个数开始倒转m个数.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+
+void reverseSubarray(int* start, int n) {
+    int* left = start;
+    int* right = start + n - 1;
+
+    while (left < right) {
+
+        int temp = *left;
+        *left = *right;
+        *right = temp;
+
+
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    int arr[10];
+
+
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+    int start, n;
+    scanf("%d %d", &start, &n);
+
+
+    int startIndex = start - 1;
+    reverseSubarray(&arr[startIndex], n);
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d", arr[i]);
+        if (i < 9) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
